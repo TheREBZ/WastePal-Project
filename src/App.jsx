28f3cb7017c42be55ddb7443fdf55e3b8468dd6e
Rebz <osazuwakelvin24@gmail.com>
2026-08-06 05:22:40 +0100

    started project. created register page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./styles/variables.css"
+import "./styles/globals.css"
+import Register from "./pages/Register"
+
+
+const App = () => {
+  return (
+    <>
+    <Register />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Register.jsx
@@ -0,0 +1,23 @@
+import "../styles/Register.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+const Register = () => {
+  return (
+    <main className='register-container'>
+        <h1>Register As</h1>
+        <section className="profile-select-container">
+            <button className="profile-btn">
+                <FontAwesomeIcon icon={faUser} className="icon"/>
+                <p>User</p>
+            </button>
+            <button className='profile-btn'>
+                <FontAwesomeIcon icon={faUser} className="icon"/>
+                <p>Waste Picker</p>
+            </button>
+        </section>
+    </main>
+  )
+}
+
+export default Register;
